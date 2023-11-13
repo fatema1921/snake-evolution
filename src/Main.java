593cfbf10f2686e42a0b6baede83f6e836dfcada
cynex0 <m.orlov10@gmail.com>
2023-11-13 14:00:30 +0100

    initial setup

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setSize(800, 800);
+        window.setLayout(null);
+        window.setVisible(true);
+    }
+}
