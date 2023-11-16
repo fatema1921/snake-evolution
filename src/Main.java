b314fd75d5c91d97bd4a4e8eaa16700363baaa4b
Victoria Rönnlid <ronnlid@net.chalmers.se>
2023-11-16 18:00:08 +0100

    menu-skeleton

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import javax.swing.*;
+import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        JFrame window = new JFrame();
-        window.setSize(800, 800);
-        window.setLayout(null);
-        window.setVisible(true);
+
+                new MainMenu();
     }
 }
