da395fe8a16665c7f6988f4ffbce49179ee16308
Maksims Orlovs <m.orlov10@gmail.com>
2023-11-16 13:18:03 +0100

    game window setup

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.awt.*;
 import java.io.*;
 import javax.swing.*;
 
 public class Main {
+    public static final Point WINDOW_SIZE = new Point(800, 800);
+    public static final int BORDER_SIZE = 40;
+    public static final int CELL_SIZE = 20;
+
     public static void main(String[] args) {
         JFrame window = new JFrame();
-        window.setSize(800, 800);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Snake Evolution");
+        window.setSize(WINDOW_SIZE.x, WINDOW_SIZE.y);
+
         window.setLayout(null);
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
 }
