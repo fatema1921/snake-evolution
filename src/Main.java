65bb8b870063a5354763e919daa80805af490ad5
Fatemeh Akbarifar <fatakb@net.chalmers.se>
2023-11-16 19:58:05 +0100

    ignore this

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+
         JFrame window = new JFrame();
         window.setSize(800, 800);
         window.setLayout(null);
         window.setVisible(true);
+
+
+
+
     }
 }
