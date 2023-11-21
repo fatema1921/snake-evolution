290756859adf1027ac8c7efe68b5a1db23fdf0c1
Maksims Orlovs <orlovs@student.chalmers.se>
2023-11-21 13:13:26 +0100

    implement CellPostition.equals()

--- src/CellPosition.java
+++ src/CellPosition.java
@@ -12,8 +12,18 @@ public class CellPosition extends Point {
         y = initCellY;
     }
 
-    public Point getCoordinates() { // returns coordinates of the current position
+    public Point getCoordinates() { // returns coordinates of the current cell
         return new Point(x * GamePanel.CELL_SIZE, y * GamePanel.CELL_SIZE);
     }
+
     public Point getCellPoint() { return new Point(x, y); }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) return false;
+        if (!(object instanceof CellPosition other)) return false;
+
+        return (this.x == other.x) &&
+                (this.y == other.y);
+    }
 }
