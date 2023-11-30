0a7c27840e5cfa764d031e9845043659f21bce32
Fatemeh Akbarifar <fatakb@net.chalmers.se>
2023-12-01 00:34:13 +0100

    food class added

---
+++ src/Food.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class Food {
+    private Point foodLocation;
+    private final int foodSize = GamePanel.CELL_SIZE;
+
+    public Food (int x , int y){
+        this.foodLocation = new CellPosition(x, y);
+    }
+
+    public void setFoodLocation (int x, int y){
+        this.foodLocation.setLocation(x, y);
+    }
+    public void draw (Graphics g) {
+        g.setColor(Color.BLUE);
+        g.drawRect(foodLocation.x, foodLocation.y, foodSize, foodSize);
+        g.setColor(Color.GRAY);
+        g.fillRect(foodLocation.x, foodLocation.y, foodSize, foodSize);
+    }
+}
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -10,6 +10,7 @@ public class GamePanel extends JPanel {
     public static final int FPS = 60;
     private final BgPanel bg;
     private Snake snake;
+    private Food food;
     private final Timer gameLoop;
 
     KeyHandler keyH = new KeyHandler(); //creating an instance of the KeyHandler abstract
@@ -29,6 +30,7 @@ public class GamePanel extends JPanel {
 
         bg = new BgPanel();
         snake = new Snake();
+        food = new Food(0, 0);
 
         gameLoop = new Timer(1000/FPS, e -> { // GAME LOOP, runs every 1/60th of a second
             update();
@@ -59,18 +61,55 @@ public class GamePanel extends JPanel {
 
     }
 
+    private void produceFood () {
+        int locX = generateRandomLoc(Main.WINDOW_SIZE.x / 20 - CELL_SIZE, 10);
+        int locY = generateRandomLoc( Main.WINDOW_SIZE.x / 20 - CELL_SIZE, 10);
+
+        food.setFoodLocation(locX,locY);
+    }
+    //Main.WINDOW_SIZE.x / 20 - 5
+    //Main.WINDOW_SIZE.x / CELL_COUNT - CELL_SIZE
+    //low: BgPanel.MARGIN_DIST + BgPanel.MARGIN_DIST
+
+    private int generateRandomLoc (int high, int low) {
+        int randomLoc = (int) (Math.floor (Math.random() * (1+high-low)) + low) * 20;
+        /*
+        while (randomLoc >= Main.WINDOW_SIZE.x - (9 * BgPanel.BORDER_SIZE + 14 * BgPanel.MARGIN_DIST) || randomLoc < BgPanel.BORDER_SIZE / 2) {
+
+            randomLoc = (int) (Math.floor (Math.random() * (1+high-low)) + low) * 20;
+        }
+         */
+        return randomLoc;
+    }
+
+
+
+    /*
+    private int generateRandomLoc () {
+
+        int randomLoc = (int) Math.random();
+
+        return  randomLoc;
+    }
+     */
+
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         bg.paintComponent(g);
 
+        food.draw(g);
+
         Graphics2D frame = (Graphics2D) g;
         snake.draw(frame);
         frame.dispose();
+
     }
 
     // starts the game loop
     public void startGame() {
         gameLoop.start();
+        produceFood();
     }
 }
