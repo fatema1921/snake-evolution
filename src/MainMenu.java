4960a334db5d3b376a9e556c588eb9e5a79d1154
Victoria Rönnlid <ronnlid@student.chalmers.se>
2023-12-06 16:13:49 +0100

    fix frame drawing bug

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -11,16 +11,14 @@ public class MainMenu extends JPanel implements ActionListener { //the mainMenu 
     private Button start; // Declaring Button references
     private Button leaderboard;
     private Button exit;
-
-    public static final int BORDER_THC = 5; // border thickness
-    public static final int MARGIN_INNER = GamePanel.CELL_SIZE * 3; // distance from screen edge to inner margin point, 3 cells away
-    public static final int MARGIN_OUTER = MARGIN_INNER - BORDER_THC; // distance from screen edge to outer margin point (closer to the screen)
+    public BgPanel panel;
 
     private ArrayList<Button> buttons;//declaring arrayList of Buttons to perform redundant button-tasks.
 
     private StateChangeListener stateChanger;
 
     public MainMenu(StateChangeListener listener) {
+        panel = new BgPanel();
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); //creates a box layout for the panel.
         this.setPreferredSize(new Dimension(GameFrame.WINDOW_SIZE.x, GameFrame.WINDOW_SIZE.y));
         this.setBackground(Color.decode("#A9E000")); // sets the color to the nokia snake green background color.
@@ -59,20 +57,7 @@ public class MainMenu extends JPanel implements ActionListener { //the mainMenu 
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        Graphics2D frame = (Graphics2D) g;
-
-        // fill background
-        frame.setColor(new Color(0xA9E000));
-        frame.fillRect(0, 0, GameFrame.WINDOW_SIZE.x, GameFrame.WINDOW_SIZE.y);
-
-        // draw borders
-        frame.setColor(Color.BLACK);
-
-        frame.fillRect(MARGIN_OUTER, MARGIN_OUTER, GameFrame.WINDOW_SIZE.x - 2 * MARGIN_OUTER, BORDER_THC); // top border
-        frame.fillRect(MARGIN_OUTER, MARGIN_OUTER, BORDER_THC, GameFrame.WINDOW_SIZE.y - 2 * MARGIN_OUTER); // left border
-        frame.fillRect(GameFrame.WINDOW_SIZE.x - MARGIN_OUTER - BORDER_THC, MARGIN_OUTER, BORDER_THC, GameFrame.WINDOW_SIZE.y - 2 * MARGIN_OUTER); // right border
-        frame.fillRect(MARGIN_OUTER, GameFrame.WINDOW_SIZE.y - MARGIN_OUTER - BORDER_THC, GameFrame.WINDOW_SIZE.x - 2 * MARGIN_OUTER, BORDER_THC); // bottom border
+        panel.paintComponent(g);
     }
 
     public void actionPerformed(ActionEvent event) { // logic for when buttons are clicked.
