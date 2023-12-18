20e270f5017f1b959028c781e68d54ed8cbd9144
Fatemeh Akbarifar <fatakb@net.chalmers.se>
2023-12-18 22:31:36 +0100

    edit speed food size

--- src/Food.java
+++ src/Food.java
@@ -42,13 +42,8 @@ public class Food {
 
     public void drawSpeedFood (Graphics2D frame) {
         Point coords = bonusFoodLocation.getCoordinates(); // top left coords of the cell
-
-        //int halfCell = GamePanel.CELL_SIZE / 2;
-        //int[] xPoints = {coords.x, coords.x + halfCell, coords.x + 2*halfCell, coords.x + halfCell};
-        //int[] yPoints = {coords.y + halfCell, coords.y, coords.y + halfCell, coords.y + 2*halfCell};
-
         frame.setColor(Color.blue);
-        frame.fillRect(coords.x, coords.y, 4, 4);
+        frame.fillRect(coords.x, coords.y, GamePanel.CELL_SIZE, GamePanel.CELL_SIZE);
     }
 
     public CellPosition getFoodLocation () {
